36340da4dd8d16d306f86437ce4dcc3508d05b13
MoeRookie <MoeRookie@163.com>
2018-08-04 08:37:59 +0800

    升级Crime类以显示陋习发生的日期及处理状态

--- app/src/main/java/com/ghsoft/criminalintent/Crime.java
+++ app/src/main/java/com/ghsoft/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.ghsoft.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,12 @@ import java.util.UUID;
 public class Crime {
     private UUID mId; // crime的唯一标识符
     private String mTitle; // 陋习名称
+    private Date mDate; // 陋习发生的时间
+    private boolean mSolved; // 陋习的处理状态
     public Crime(){
         // 生成唯一标识符
         mId = UUID.randomUUID();
+        mDate = new Date(); // 陋习的发生时间默认为当前时间
     }
 
     public UUID getId() {
@@ -24,5 +28,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
