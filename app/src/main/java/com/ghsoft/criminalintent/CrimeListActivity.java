608211207caafc92554db9851f8ca27c927b2e8d
MoeRookie <MoeRookie@163.com>
2018-08-05 15:44:01 +0800

    新建CrimeListActivity和CrimeListFragment控制类,并声明CrimeListActivity为launcher activity

---
+++ app/src/main/java/com/ghsoft/criminalintent/CrimeListActivity.java
@@ -0,0 +1,14 @@
+package com.ghsoft.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by mac on 2018/8/5.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ app/src/main/java/com/ghsoft/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.ghsoft.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by mac on 2018/8/5.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
