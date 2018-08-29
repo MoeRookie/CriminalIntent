51b525d20491df6f457bf421db0825cf1ef3bf75
MoeRookie <MoeRookie@163.com>
2018-08-29 10:12:55 +0800

    定义描述scrime记录的表元素(表名、字段名)

---
+++ app/src/main/java/com/ghsoft/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,21 @@
+package com.ghsoft.criminalintent.database;
+
+/**
+ * Created by mac on 2018/8/29.
+ */
+
+public class CrimeDbSchema {
+    // crime记录表
+    // 定义描述表元素的String常量
+    public static final class CrimeTable{
+        // 表名
+        public static final String NAME = "crimes";
+        // 字段名/列名
+        public static final class Cols{
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
