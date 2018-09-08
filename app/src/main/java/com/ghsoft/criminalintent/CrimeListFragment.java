cbdb4e0aeaeadf4d14cf9bbb9c20f3e4c213fe57
MoeRookie <MoeRookie@163.com>
2018-09-08 10:08:30 +0800

    修改updateUI()方法的可见性

--- app/src/main/java/com/ghsoft/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/ghsoft/criminalintent/CrimeListFragment.java
@@ -92,7 +92,7 @@ public class CrimeListFragment extends Fragment {
         updateUI();
     }
 
-    private void updateUI() {
+    public void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
         boolean isCrimeEmpty = crimes.size() != 0;
